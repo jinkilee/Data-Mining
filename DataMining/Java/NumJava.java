e78b1ceb5c93d8b3869229700c9bd24f96f436ac
jkfirst <jinkilee73@gmail.com>
2016-07-07 12:59:49 +0100

    Commit 160707

--- DataMining/Java/NumJava.java
+++ DataMining/Java/NumJava.java
@@ -1,13 +1,31 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 
 public class NumJava {
-	// generic method printArray                         
-	public static <T extends Number> float average(T[] data) {
-		// Display array elements
-		float sum = 0;
-		int dataLen = data.length;
-		for(int i = 0; i < dataLen; i++)
-			sum += data[i].floatValue();
-		return sum/dataLen;
+	// Get Average of Number list
+	// Input  : List<T extends Number>
+	// Output : Double 			# To be precise
+	public static <T extends Number> Double average(List<T> data) {
+		Double sum = 0.0;
+		for(T elem:data) {
+			sum += elem.floatValue();
+		}
+		return sum/data.size();
+	}
+
+	// Get Maxinum of data
+	// Input  : List<T extends Number>
+	// Output : T
+	public static <T extends Number> Number max(List<T> data) {
+		Number max = 0;
+		for(T elem:data) {
+			if(max.doubleValue() > elem.doubleValue())
+				continue;
+			max = elem.floatValue();
+		}
+
+		//return Collections.max(data, null);
+		return max;
 	}
 }
--- DataMining/Java/Test.java
+++ DataMining/Java/Test.java
@@ -1,9 +1,18 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
 
 public class Test {
 	public static void main(String[] args) {
-		Number[] data = {1.1, 2, 3, 4, 5};
+		Number[] data = {1, 2, 3, 4, 5.1};
+		List<Number> list = Arrays.asList(data);
+
+		//*/
+		// Get average
 		NumJava nj = new NumJava();
-		System.out.println(nj.average(data));
+		//System.out.println(nj.average(list));
+		//System.out.println(nj.max(list));
+		//*/
 	}
 }
