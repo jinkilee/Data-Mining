3b6359d715529a94184f78f0ea33cd8777cefc14
jinki <jinkilee73@gmail.com>
2016-10-13 23:53:40 +0900

    commit 161013

--- DataMining/Python/svm.py
+++ DataMining/Python/svm.py
@@ -1,5 +1,3 @@
-print(__doc__)
-
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import svm
@@ -9,37 +7,56 @@ np.random.seed(0)
 X = np.r_[np.random.randn(20, 2) - [2, 2], np.random.randn(20, 2) + [2, 2]]
 Y = [0] * 20 + [1] * 20
 
-X = [[2,2,1],[1,2,2],[-1,2,1]]
-X = np.array(X)
-Y = [1,1,-1]
+# figure number
+fignum = 1
 
 # fit the model
-clf = svm.SVC(kernel='linear')
-clf.fit(X, Y)
+for name, penalty in (('unreg', 1), ('reg', 0.05)):
+	clf = svm.SVC(kernel='linear', C=penalty)
+	clf.fit(X, Y)
 
-# get the separating hyperplane
-w = clf.coef_[0]
-a = -w[0] / w[1]
-print(w)
+	# get the separating hyperplane
+	w = clf.coef_[0]
+	a = -w[0] / w[1]
+	xx = np.linspace(-5, 5)
+	yy = a * xx - (clf.intercept_[0]) / w[1]
 
-xx = np.linspace(-5, 5)
-yy = a * xx - (clf.intercept_[0]) / w[1]
+	# plot the parallels to the separating hyperplane that pass through the
+	# support vectors
+	margin = 1 / np.sqrt(np.sum(clf.coef_ ** 2))
+	yy_down = yy + a * margin
+	yy_up = yy - a * margin
 
-# plot the parallels to the separating hyperplane that pass through the
-# support vectors
-b = clf.support_vectors_[0]
-yy_down = a * xx + (b[1] - a * b[0])
-b = clf.support_vectors_[-1]
-yy_up = a * xx + (b[1] - a * b[0])
+	# plot the line, the points, and the nearest vectors to the plane
+	plt.figure(fignum, figsize=(4, 3))
+	plt.clf()
+	plt.plot(xx, yy, 'k-')
+	plt.plot(xx, yy_down, 'k--')
+	plt.plot(xx, yy_up, 'k--')
 
-# plot the line, the points, and the nearest vectors to the plane
-plt.plot(xx, yy, 'k-')
-plt.plot(xx, yy_down, 'k--')
-plt.plot(xx, yy_up, 'k--')
+	plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1], s=80, facecolors='none', zorder=10)
+	plt.scatter(X[:, 0], X[:, 1], c=['red', 'blue'], zorder=10, cmap=plt.cm.Paired)
+	#plt.scatter(X[:, 0], X[:, 1], c=Y, zorder=10, cmap=plt.cm.Paired)
 
-plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1],
-            s=80, facecolors='none')
-plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=plt.cm.Paired)
+	plt.axis('tight')
+	x_min = -4.8
+	x_max = 4.2
+	y_min = -6
+	y_max = 6
 
-plt.axis('tight')
+	XX, YY = np.mgrid[x_min:x_max:200j, y_min:y_max:200j]
+	Z = clf.predict(np.c_[XX.ravel(), YY.ravel()])
+
+	# Put the result into a color plot
+	Z = Z.reshape(XX.shape)
+	plt.figure(fignum, figsize=(4, 3))
+	plt.pcolormesh(XX, YY, Z, cmap=plt.cm.Paired)
+
+	plt.xlim(x_min, x_max)
+	plt.ylim(y_min, y_max)
+
+	plt.xticks(())
+	plt.yticks(())
+	fignum = fignum + 1
+
 plt.show()
