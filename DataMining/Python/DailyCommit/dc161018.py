d985c23e99c1d5fa4c02fa33f1165e903e4a3c23
jinki <jinkilee73@gmail.com>
2016-10-19 15:13:05 +0900

    commit 161019

---
+++ DataMining/Python/DailyCommit/dc161018.py
@@ -0,0 +1,24 @@
+# for drawing pdf
+import os
+from sklearn import tree
+
+# others...
+from sklearn.datasets import load_iris
+
+def draw_tree(model, dot_file):
+	with open(dot_file, 'w') as f:
+		f = tree.export_graphviz(model, out_file=f)
+		
+	pdf_file = dot_file.split(".")[0] + ".pdf"
+	cmd = "dot -Tpdf " + dot_file + " -o " + pdf_file
+
+	return
+
+# build decision tree model
+iris = load_iris()
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(iris.data, iris.target) # fit(x, y)
+
+# drawing pdf
+draw_tree(clf, "iris.dot")
+os.system('dot -Tpdf iris.dot -o iris.pdf')
