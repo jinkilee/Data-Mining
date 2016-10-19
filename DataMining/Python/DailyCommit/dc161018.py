fe239067c668382607d4a2afa3fbd1e0a7227ea1
jinki <jinkilee73@gmail.com>
2016-10-19 16:51:57 +0900

    commit 161018

--- DataMining/Python/DailyCommit/dc161018.py
+++ DataMining/Python/DailyCommit/dc161018.py
@@ -7,7 +7,7 @@ from sklearn.datasets import load_iris
 
 def draw_tree(model, dot_file):
 	with open(dot_file, 'w') as f:
-		f = tree.export_graphviz(model, out_file=f)
+		f = tree.export_graphviz(model, out_file=f, feature_names=iris.feature_names, class_names=iris.target_names, filled=True, rounded=True, special_characters=True)
 		
 	pdf_file = dot_file.split(".")[0] + ".pdf"
 	cmd = "dot -Tpdf " + dot_file + " -o " + pdf_file
